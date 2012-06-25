170b31b219a4b483b340cda56df2632528031d82
jinwyp <jinwyp@gmail.com>
2012-06-25 17:57:53 +0800

    学习ember js

--- js/myapp.js
+++ js/myapp.js
@@ -7,14 +7,26 @@
 // ==========================================================================
 
 
+
+
 // add a namespace
-var App = Em.Application.create();
+var App = Ember.Application.create({
+	mixmaster: 'Andy', 
+	totalReviews: 0, 
+
+});
+
+App.PeopleView = Ember.View.create({
+  people: [ { name: 'Yehuda' },
+            { name: 'Tom' } ]
+});
 
 // Example 1
-App.president = Em.Object.create({
+App.president = Ember.Object.create({
     firstName: "Barack",
     lastName: "Obama",
-    name: "xxx xxx" ,
+    name: 0 ,
+	
     fullName: function() {
         return this.get('firstName') + ' ' + this.get('lastName');
         // Tell Ember that this computed property depends on firstName
@@ -22,23 +34,34 @@ App.president = Em.Object.create({
     }.property('firstName', 'lastName')
 });
 
+
+
 $('#btn1').click( function() {
     App.president.set('lastName', 'Obamaomao');
+	App.president.firstName = 1;
+	console.log(App.president.name); 
 });
 
 
 // Example 2
-App.InfoView = Em.View.extend({
+App.InfoView = Ember.View.extend({
     templateName: 'info',
     posts: 25,
     hobbies: "Riding bicycles"
 });
-App.InfoView.create().appendTo('#temp-wrap');
 
-// or
-// var info = App.InfoView.create();
-// info.appendTo('#temp-wrap');
+var info1 = App.InfoView.create({
+	posts: 27
+});
+console.log(info1.posts); 
 
+App.ReviewTextArea = Ember.TextArea.extend({
+	placeholder: 'Enter your review' 
+});
+
+$('#btn22').click( function() {
+    info.appendTo('#temp11');
+});
 
 // Example 3
 App.country = Ember.Object.create({
