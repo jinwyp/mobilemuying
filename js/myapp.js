4712307d80f8929f28a78df0de7e5e6587a0d3e7
zcf1250 <zcf1250@muyingzhijia.com>
2012-07-05 15:06:15 +0800

    0705-2
    登录和注册页增加验证

--- js/myapp.js
+++ js/myapp.js
@@ -1,86 +1,33 @@
-/**************************
- * Application
- **************************/
-
-App = Em.Application.create();
-
-/**************************
- * Models
- **************************/
-App.Tweet = Em.Object.extend({
-    avatar: null,
-    screen_name: null,
-    text: null,
-    date: null
-});
-
-/**************************
- * Views
- **************************/
-
-App.SearchTextField = Em.TextField.extend({
-        insertNewline: function(){
-            App.tweetsController.loadTweets();
-        }
-});
-
-/**************************
- * Controllers
- **************************/
-
-App.tweetsController = Em.ArrayController.create({
-    content: [],
-    username: '',
-    loadTweets: function() {
-        var me = this;
-        var username = me.get("username");
-
-        if ( username ) {
-            var url = 'http://api.twitter.com/1/statuses/user_timeline.json'
-                url += '?screen_name=%@&callback=?'.fmt(me.get("username"));
-            // push username to recent user array
-            App.recentUsersController.addUser(username);
-            $.getJSON(url,function(data){
-                me.set('content', []);
-                $(data).each(function(index,value){
-                    var t = App.Tweet.create({
-                        avatar: value.user.profile_image_url,
-                        screen_name: value.user.screen_name,
-                        text: value.text,
-                        date: value.created_at
-                    });
-                    me.pushObject(t);
-                })
-            });
-        }
-    }
-});
-
-App.recentUsersController = Em.ArrayController.create({
-    content: [],
-    addUser: function(name) {
-        if ( this.contains(name) ) this.removeObject(name);
-        this.pushObject(name);
-    },
-    removeUser: function(view){
-        this.removeObject(view.context);
-    },
-    searchAgain: function(view){
-        App.tweetsController.set('username', view.context);
-        App.tweetsController.loadTweets();
-    },
-    reverse: function(){
-        return this.toArray().reverse();
-    }.property('@each')
-});
-
-//console.log(App.tweetsController.content);
-
-
 
 
 $("#frmLogin").validate({
 
+    rules: {
+        email: {
+            required: true,
+            email: true
+        },
+        password: {
+            required: true,
+            minlength: 6,
+            maxlength: 20
+        }
+    },
+
+    messages: {
+        email: {
+            required: "请输入Email地址",
+            email: "请输入正确的email地址"
+        },
+        password: {
+            required: "请输入密码",
+            minlength: jQuery.validator.format("密码不能小于{0}个字符"),
+            maxlength: jQuery.validator.format("密码不能最多超过{0}的字符")
+        }
+    }
+});
+
+$("#frmReg").validate({
     rules: {
         email: {
             required: true,
@@ -117,6 +64,5 @@ $("#frmLogin").validate({
         }
     }
 });
-
 
 
