50acf85643f3709c36c45cc018678e00d01645c5
CLOCK <zcf1250@muyingzhijia.com>
2012-07-25 15:01:13 +0800

    0725
    修正注册时同意协议提示位置错位的问题

--- js/my.validate.js
+++ js/my.validate.js
@@ -26,46 +26,50 @@ jQuery("#frmLogin").validate({
 
 jQuery("#frmReg").validate({
     errorElement:"span",
-    rules: {
-        email: {
-            required: true,
-            email: true
+    rules:{
+        email:{
+            required:true,
+            email:true
         },
-        password: {
-            required: true,
-            minlength: 6,
-            maxlength: 20
+        password:{
+            required:true,
+            minlength:6,
+            maxlength:20
         },
-        confirm_password: {
-            required: true,
-            minlength: 6,
-            maxlength: 20,
-            equalTo: "#password"
+        confirm_password:{
+            required:true,
+            minlength:6,
+            maxlength:20,
+            equalTo:"#password"
         },
-        mobile: {
-            required: true,
-            number: true}
+        mobile:{
+            required:true,
+            number:true}
     },
 
-    messages: {
-        email: {
-            required: "请输入Email地址",
-            email: "请输入正确的email地址"
+    messages:{
+        email:{
+            required:"请输入Email地址",
+            email:"请输入正确的email地址"
         },
-        password: {
-            required: "请输入密码",
-            minlength: jQuery.validator.format("密码不能小于{0}个字符"),
-            maxlength: jQuery.validator.format("密码不能最多超过{0}的字符")
+        password:{
+            required:"请输入密码",
+            minlength:jQuery.validator.format("密码不能小于{0}个字符"),
+            maxlength:jQuery.validator.format("密码不能最多超过{0}的字符")
         },
-        confirm_password: {
-            required: "请输入确认密码",
-            minlength: jQuery.validator.format("确认密码不能小于{0}个字符"),
-            maxlength: jQuery.validator.format("密码不能最多超过{0}的字符"),
-            equalTo: "两次输入密码不一致"
+        confirm_password:{
+            required:"请输入确认密码",
+            minlength:jQuery.validator.format("确认密码不能小于{0}个字符"),
+            maxlength:jQuery.validator.format("密码不能最多超过{0}的字符"),
+            equalTo:"两次输入密码不一致"
         },
-        mobile: {
-            required: "请输入手机号",
-            number: "请输入有效的手机号码"
+        mobile:{
+            required:"请输入手机号",
+            number:"请输入有效的手机号码"
+
+        },
+        checkbox_agree:{
+            required:"您还没有同意母婴之家”注册条款“！"
 
         }
     }
