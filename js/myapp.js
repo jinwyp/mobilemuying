98af6bc1573f004fbc14cb33086e3e8a37dec700
Jin <jinwyp@gmail.com>
2012-09-04 16:21:08 +0800

    0904
    $.mobile.changePage(); 就可以刷新页面 可以看登录页面 点击上面的分类图片

--- js/myapp.js
+++ js/myapp.js
@@ -2,3 +2,24 @@ jQuery('#gotop').tap(function(){
     $.mobile.silentScroll(10);
 });
 
+jQuery( '#categorypage' ).bind( 'tap', function( event ){
+
+    $.mobile.changePage();
+
+});
+
+
+jQuery( '#myaccountpage' ).bind( 'tap', function( event ){
+
+    $.mobile.changePage("myaccount.html", { transition : "slide" });
+
+});
+
+jQuery( '#shoppingcartpage' ).bind( 'tap', function( event ){
+
+    $.mobile.changePage("shoppingcart.html", { transition : "slide" });
+
+});
+
+
+//$('#category').trigger('create');
